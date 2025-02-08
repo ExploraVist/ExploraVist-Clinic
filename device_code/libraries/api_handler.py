dffb962b8638ec4d8481d6753261822fe6761fa5
Ket Hollingsworth <khollingsworth@g.hmc.edu>
2025-02-07 20:48:51 -0800

    print audio file name when error

--- device_code/libraries/api_handler.py
+++ device_code/libraries/api_handler.py
@@ -100,7 +100,7 @@ class APIHandler:
                         audio_file (str): Path to the audio file to play
                 """
                 if not os.path.exists(audio_file):
-                        print("Error: Audio file not found.")
+                        print(F"Error: Audio file not found: {audio_file}")
                         return
 
                 print(f"Audio data received successfully. Playing audio: {audio_file}")
