e4d6cebe4c43c110e4d8f88e06a1b6ca44097338
Ket Hollingsworth <khollingsworth@g.hmc.edu>
2025-04-09 00:24:07 -0700

    changed GPIO

--- device_code/main.py
+++ device_code/main.py
@@ -32,8 +32,8 @@ def main():
 
     # Set pin 22 to pull up (normally closed)
     GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    # Set pin 23 to pul up (normally closed)
-    GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    # Set pin 27 to pul up (normally closed)
+    GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     # Shutdown Pin on Amplifier
     AMP_SD_PIN = 26
@@ -55,11 +55,11 @@ def main():
         button_pressed = 0
 
         start_time = time.time()
-        while GPIO.input(22) == GPIO.LOW or GPIO.input(23) == GPIO.LOW:
+        while GPIO.input(22) == GPIO.LOW or GPIO.input(27) == GPIO.LOW:
             device.start_recording()
             if GPIO.input(22) == GPIO.LOW:
                 button_pressed = 2
-            elif GPIO.input(23) == GPIO.LOW:
+            elif GPIO.input(27) == GPIO.LOW:
                 button_pressed = 1
 
         device.stop_recording()
