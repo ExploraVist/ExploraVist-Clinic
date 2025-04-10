2056770e6464dec28b5b24750d6c90f1a5496f0f
tlevn22 <tlevinson@g.hmc.edu>
2025-04-10 00:30:09 -0700

    ooops

--- device_code/libraries/api_handler.py
+++ device_code/libraries/api_handler.py
@@ -434,6 +434,7 @@ class APIHandler:
                 url = "https://api.deepgram.com/v1/speak"
                 headers = {
                         "Authorization": f"Token {self.DEEPGRAM_API_KEY}",
+                        "Content-Type": "text/plain",
                         "Accept": "audio/mpeg"
                 }
 
