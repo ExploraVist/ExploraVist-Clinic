944039f86f2e40f592eebf5acfffaf12264c49f6
Ket Hollingsworth <khollingsworth@g.hmc.edu>
2025-04-10 22:42:35 -0700

    fixed gpt call

--- device_code/main.py
+++ device_code/main.py
@@ -156,7 +156,7 @@ def main():
                 temp_prompt = context_window + f"Current Question: {transcript} \n"
 
                 # Make LLM API Call with Custom Prompt
-                text_response = api_handler.gpt_request2(temp_prompt)
+                text_response = api_handler.gpt_image_request2(temp_prompt)
                 context_window += f"USER: {transcript} \n GPT: {text_response} \n"
 
                 # Check that pin is low
